a9d8bc88cf8cd1ca773f4f2f1cb2b0c6654d8dab
nitanilla <nitanilla@gmail.com>
2015-09-17 04:57:55 -0300

    parsing right

--- server.rb
+++ server.rb
@@ -5,6 +5,12 @@ require "typhoeus"
 class Server < Sinatra::Base
   get "/*" do
     url = "https://api.github.com/" + params[:splat][0]
-    Typhoeus.get(url).body
+    uri = URI.parse(url)
+    new_params = params.to_hash
+    new_params.delete('splat')
+    new_params.delete('captures')
+    uri.query = URI.escape(new_params.map{|key,val| "#{key}=#{val}"}.join("&")) unless new_params.empty?
+    p uri.to_s
+    Typhoeus.get(uri.to_s).body
   end
 end
